877505ff2789a8b1dc5d5d360283c582189a95a0
Juliana Carvalho <juliana.vcarvalhoo@gmail.com>
2022-03-08 22:48:00 -0300

    Basics statics components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Ola</h1>
+
+      <DataTable />
     </div>
+    <Footer />
+    </>
+    
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light"> Desenvolvido por <a href="https://github.com/julianavcarvalho" target="_blank" rel="noreferrer">Juliana Vieira de Carvalho</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      </small></p>
+  </div>
+</footer>
+  );
+}
+
+export default Footer;
